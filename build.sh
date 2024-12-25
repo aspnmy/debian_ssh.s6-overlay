8601bb89905c8ed0ef4d5270289131b0a35f70da
nasAdmin <support@e2bank.cn>
2024-12-25 16:04:23 +0000

    进一步减小镜像体积

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
 #!bin/bash
-URI="docker.io"
-AUUSER="aspnmy"
-imgNAME="debian-ssh"
-ver="s6-overlay-v12.7-sshkey-rc1"
-docker build --no-cache -f ./dockerfile/dockerfile-ssh-s6-overlay-addkeys \
-        -t $URI/$AUUSER/$imgNAME:$ver .
+bash ./dockerfile/build.sh
