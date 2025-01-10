eff5f8435cc97f3f0de68e0129c8d7b15619d3e1
aspnmy <support@e2bank.cn>
2025-01-11 02:02:14 +0800

    更新cf加速组件

--- Dockerfile/build-devbash.sh
+++ Dockerfile/build-devbash.sh
@@ -1,4 +1,10 @@
 #!bin/bash
+
+# Build the image
+# docker build --no-cache -f ./dockerfile-ssh-s6-overlay-devwork-base -t docker.io/aspnmy/debian-ssh:s6-overlay-v12.7-devworker-bash .
+# docker push docker.io/aspnmy/debian-ssh:s6-overlay-v12.7-devworker-bash
+# 更新支持CF测试工具的镜像
+# time="2025-01-11 00:00:00" 
 URI="docker.io"
 AUUSER="aspnmy"
 imgNAME="debian-ssh"
