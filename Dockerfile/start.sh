e7412ca8551acb79d97cff751a3e8ab61555de56
春秋老怪 <aspnmy@users.noreply.github.com>
2025-01-10 23:04:14 +0800

    Update start.sh

--- Dockerfile/start.sh
+++ Dockerfile/start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # 启动ssh-server
 
-# mkdir -p /aspnmy/wwwroot/ssh_key
-# echo "重置root-sshkey..."
-# chmod +x /etc/ssh/setRootKey_Cli
-# /etc/ssh/setRootKey_Cli usrkey
+mkdir -p /aspnmy/wwwroot/ssh_key
+echo "重置root-sshkey..."
+chmod +x /etc/ssh/setRootKey_Cli
+/etc/ssh/setRootKey_Cli usrkey
 
 echo "Starting ssh-server..."
 /usr/sbin/sshd -D
