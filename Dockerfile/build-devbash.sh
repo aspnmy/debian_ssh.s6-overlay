b234a0c411e7b3a7abc957d25c23f5bbd0611f8a
aspnmy <support@e2bank.cn>
2025-01-10 22:49:59 +0800

    up

--- Dockerfile/build-devbash.sh
+++ Dockerfile/build-devbash.sh
@@ -3,5 +3,5 @@ URI="docker.io"
 AUUSER="aspnmy"
 imgNAME="debian-ssh"
 ver="s6-overlay-v12.7-devworker-bash"
-docker build  -f ./dockerfile-ssh-s6-overlay-addkeys \
+docker build --no-cache -f ./dockerfile-ssh-s6-overlay-addkeys \
         -t $URI/$AUUSER/$imgNAME:$ver .
