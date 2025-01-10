0c788aa53dde9379c563d5e585bd70f797632ef2
aspnmy <support@e2bank.cn>
2025-01-10 22:42:12 +0800

    更新只用root工具管理的版本，取消纯密码管理，增加安全性

--- Dockerfile/build-devbash.sh
+++ Dockerfile/build-devbash.sh
@@ -2,6 +2,6 @@
 URI="docker.io"
 AUUSER="aspnmy"
 imgNAME="debian-ssh"
-ver="s6-overlay-v12.7-sshkey-rc1"
-docker build --no-cache -f ./dockerfile-ssh-s6-overlay-addkeys \
+ver="s6-overlay-v12.7-devworker-bash"
+docker build  -f ./dockerfile-ssh-s6-overlay-addkeys \
         -t $URI/$AUUSER/$imgNAME:$ver .
