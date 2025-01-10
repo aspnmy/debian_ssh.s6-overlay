f8431372a2da94d657239e816eb9ab15397ecf31
春秋老怪 <aspnmy@users.noreply.github.com>
2025-01-11 04:06:10 +0800

    Update build-devbash.sh

--- Dockerfile/build-devbash.sh
+++ Dockerfile/build-devbash.sh
@@ -8,8 +8,10 @@
 URI="docker.io"
 AUUSER="aspnmy"
 imgNAME="debian-ssh"
-ver="s6-overlay-v12.7-devworker-bash"
-docker build --no-cache -f ./dockerfile-ssh-s6-overlay-devwork-base \
+imgNAMEBase="debian"
+ver="s6-overlay-v3.2.0.2-v12.7-devbase"
+docker build --no-cache -f ./dockerfile-ssh-s6-overlay \
         -t $URI/$AUUSER/$imgNAME:$ver .
-
+docker tag $URI/$AUUSER/$imgNAME:$ver $URI/$AUUSER/$imgNAMEBase:$ver
 docker push $URI/$AUUSER/$imgNAME:$ver
+docker push $URI/$AUUSER/$imgNAMEBase:$ver
