d6ccf0ef5a19c9353c7edd71f9ac92a21eeaf457
春秋老怪 <aspnmy@users.noreply.github.com>
2025-01-10 23:32:07 +0800

    Create build-devwork-cn.sh

---
+++ Dockerfile/build-devwork-cn.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+URI="docker.io"
+AUUSER="aspnmy"
+imgNAME="debian-ssh"
+ver="s6-overlay-v12.7-devworker-cn"
+docker build --no-cache -f ./dockerfile-ssh-s6-overlay-devwork \
+        -t $URI/$AUUSER/$imgNAME:$ver .
+
+docker push $URI/$AUUSER/$imgNAME:$ver
