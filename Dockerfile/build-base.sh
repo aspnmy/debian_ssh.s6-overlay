ec1e26a18cf2cf67b00593b419bb352e0352b30a
春秋老怪 <aspnmy@users.noreply.github.com>
2025-01-11 03:27:55 +0800

    Update build-base.sh

--- Dockerfile/build-base.sh
+++ Dockerfile/build-base.sh
@@ -3,6 +3,6 @@ URI="docker.io"
 AUUSER="aspnmy"
 imgNAME="debian-ssh"
 ver="s6-overlay-v12.7-devwork"
-docker build --no-cache -f ./dockerfile-ssh-s6-overlay-devwork \
+docker build --no-cache -f ./dockerfile-ssh-s6-overlay \
         -t $URI/$AUUSER/$imgNAME:$ver .
 docker push $URI/$AUUSER/$imgNAME:$ver
