9e7cf1c2c3fefd258bd2400b02049759fc18bf43
彩霞姐姐 <support@e2bank.cn>
2025-09-23 15:50:53 +0000

    build(Dockerfile): 更新基础镜像版本和构建脚本
    更新基础镜像版本为aspnmy/debian-ssh:table-20250908-slim_baseimage
    修改构建脚本中的版本号生成逻辑，使用当前时间作为版本后缀

--- Dockerfile/build-stable-slim.sh
+++ Dockerfile/build-stable-slim.sh
@@ -2,9 +2,9 @@
 URI="docker.io"
 AUUSER="aspnmy"
 imgNAME="debian-ssh"
-debianVer="stable-20250908-slim"
 timeBuild=$(date +"%Y%m%d%H")
-ver="${timeBuild}_${debianVer}_baseimage"
+debianVer="stable-${timeBuild}-slim"
+ver="${debianVer}_baseimage"
 buildah bud --no-cache -f ./dockerfile-debian-stable-slim -t $URI/$AUUSER/$imgNAME:$ver 
 buildah tag $URI/$AUUSER/$imgNAME:$ver
 buildah push $URI/$AUUSER/$imgNAME:$ver
