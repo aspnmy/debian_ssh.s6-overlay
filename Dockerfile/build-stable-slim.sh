b398260c71bdfe22c4f68cf025f5dd6ce81897b8
彩霞姐姐 <support@e2bank.cn>
2025-09-23 15:34:42 +0000

    fix: 修复构建版本号顺序问题
    确保在生成版本号之前先获取当前日期，以避免使用未定义的变量

--- Dockerfile/build-stable-slim.sh
+++ Dockerfile/build-stable-slim.sh
@@ -3,8 +3,8 @@ URI="docker.io"
 AUUSER="aspnmy"
 imgNAME="debian-ssh"
 debianVer="stable-20250908-slim"
-ver="${timeBuild}_${debianVer}_baseimage"
 timeBuild=$(date +"%Y%m%d")
+ver="${timeBuild}_${debianVer}_baseimage"
 buildah bud --no-cache -f ./dockerfile-debian-stable-slim -t $URI/$AUUSER/$imgNAME:$ver 
 buildah tag $URI/$AUUSER/$imgNAME:$ver
 buildah push $URI/$AUUSER/$imgNAME:$ver
