6c3534e5f0c8f6bcd06b6d0110dc4c1a7a2ca941
彩霞姐姐 <support@e2bank.cn>
2025-09-23 16:41:13 +0000

    refactor(Dockerfile): 重构镜像构建脚本和参数化基础镜像
    重构Dockerfile以支持参数化基础镜像标签
    删除旧构建脚本并新增模块化构建脚本
    优化镜像版本命名规则和构建流程

--- Dockerfile/build-base-BestHostsMonitor.sh
+++ Dockerfile/build-base-BestHostsMonitor.sh
@@ -5,10 +5,10 @@ imgNAME="debian-ssh"
 s6OverlayVer="3210"
 debianVer="stable-20250908-slim"
 timeBuild=$(date +"%Y%m%d%H")
-ver="${timeBuild}_s6_overlay_v${s6OverlayVer}_${debianVer}_BestHostsMonitor"
-stableVer="stable_s6-overlay_v${s6OverlayVer}_${debianVer}_BestHostsMonitor"
+#ver="${timeBuild}_s6_overlay_v${s6OverlayVer}_${debianVer}_BestHostsMonitor"
+stableVer="stable_${timeBuild}-s6-overlay_v${s6OverlayVer}_${debianVer}_BestHostsMonitor"
 
-buildah bud --no-cache -f  ./dockerfile-ssh-s6-overlay-BestHostsMonitor -t $URI/$AUUSER/$imgNAME:$ver
-buildah tag $URI/$AUUSER/$imgNAME:$ver $URI/$AUUSER/$imgNAME:$stableVer
-buildah push $URI/$AUUSER/$imgNAME:$ver
+buildah bud --no-cache -f  ./dockerfile-ssh-s6-overlay-BestHostsMonitor -t $URI/$AUUSER/$imgNAME:$stableVer
+#buildah tag $URI/$AUUSER/$imgNAME:$ver $URI/$AUUSER/$imgNAME:$stableVer
+#buildah push $URI/$AUUSER/$imgNAME:$ver
 buildah push $URI/$AUUSER/$imgNAME:$stableVer
---
+++ Dockerfile/build-workbase.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+    URI="docker.io"
+    AUUSER="aspnmy"
+    imgNAME="debian-ssh"
+    s6OverlayVer="3210"
+    debianVer="stable-20250908-slim"
+    timeBuild=$(date +"%Y%m%d%H")
+    #ver="${timeBuild}_s6-overlay_v${s6OverlayVer}_${debianVer}"
+    stableVer="stable_${timeBuild}-s6-overlay_v${s6OverlayVer}_${debianVer}"
+
+    buildah bud --no-cache -f  ./dockerfile-ssh-s6-overlay-baseimage -t $URI/$AUUSER/$imgNAME:$stableVer 
+    #buildah tag $URI/$AUUSER/$imgNAME:$ver $URI/$AUUSER/$imgNAME:$stableVer
+    #buildah push $URI/$AUUSER/$imgNAME:$ver
+    buildah push $URI/$AUUSER/$imgNAME:$stableVer 
+    return "$URI/$AUUSER/$imgNAME:$stableVer"
