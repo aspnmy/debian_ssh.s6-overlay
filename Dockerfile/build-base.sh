3e0f98f25bde2fa7f5cc14b98c4e9f972c6bb3bb
彩霞姐姐 <support@e2bank.cn>
2025-09-23 14:38:03 +0000

    refactor(Dockerfile): 简化时间戳格式为年月日
    将构建时间戳从完整格式"年月日时分秒"简化为"年月日"格式，减少冗余信息

--- Dockerfile/build-base.sh
+++ Dockerfile/build-base.sh
@@ -4,8 +4,8 @@ AUUSER="aspnmy"
 imgNAME="debian-ssh"
 s6OverlayVer="3210"
 debianVer="stable-20250908-slim"
-timeBuild=$(date +"%Y%m%d%H%M%S")
-ver="${timeBuild}_s6_overlay_v${s6OverlayVer}_${debianVer}"
+timeBuild=$(date +"%Y%m%d")
+ver="${timeBuild}_s6-overlay_v${s6OverlayVer}_${debianVer}"
 
 buildah bud --no-cache -f  ./dockerfile-ssh-s6-overlay-baseimage -t $URI/$AUUSER/$imgNAME:$ver 
 buildah push $URI/$AUUSER/$imgNAME:$ver
