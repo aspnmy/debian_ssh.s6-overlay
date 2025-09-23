38f4c9456e33af628435661071aacdd46189eb60
彩霞姐姐 <support@e2bank.cn>
2025-09-23 14:18:33 +0000

    build(Dockerfile): 在镜像版本中添加构建时间戳以区分构建
    在镜像版本号中添加构建时间戳，确保每次构建生成的镜像版本唯一，便于追踪和管理

--- Dockerfile/build-base.sh
+++ Dockerfile/build-base.sh
@@ -4,7 +4,8 @@ AUUSER="aspnmy"
 imgNAME="debian-ssh"
 s6OverlayVer="v3.2.1.0"
 debianVer="stable-20250908-slim"
-ver="s6_overlay_v${s6OverlayVer}_${debianVer}_baseimage"
+timeBuild=$(date +"%Y%m%d%H%M%S")
+ver="${timeBuild}_s6_overlay_v${s6OverlayVer}_${debianVer}_baseimage"
 
 buildah bud --no-cache -f  ./dockerfile-ssh-s6-overlay-baseimage -t $URI/$AUUSER/$imgNAME:$ver 
 buildah push $URI/$AUUSER/$imgNAME:$ver
