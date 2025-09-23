cf76e892d77448314529c490dd902207d35bde17
彩霞姐姐 <support@e2bank.cn>
2025-09-23 15:21:59 +0000

    fix(Dockerfile): 更新基础镜像构建脚本和依赖
    更新基础镜像构建脚本，添加git工具并清理临时文件
    修改版本标签命名规则，移除测试标记

--- Dockerfile/build-base.sh
+++ Dockerfile/build-base.sh
@@ -7,7 +7,7 @@ debianVer="stable-20250908-slim"
 timeBuild=$(date +"%Y%m%d")
 
 ver="${timeBuild}_s6-overlay_v${s6OverlayVer}_${debianVer}"
-stableVer="test-stable_s6-overlay_v${s6OverlayVer}_${debianVer}"
+stableVer="stable_s6-overlay_v${s6OverlayVer}_${debianVer}"
 
 buildah bud --no-cache -f  ./dockerfile-ssh-s6-overlay-baseimage -t $URI/$AUUSER/$imgNAME:$ver 
 buildah tag $URI/$AUUSER/$imgNAME:$ver $URI/$AUUSER/$imgNAME:$stableVer
--- Dockerfile/build-stable-slim.sh
+++ Dockerfile/build-stable-slim.sh
@@ -4,7 +4,7 @@ AUUSER="aspnmy"
 imgNAME="debian-ssh"
 debianVer="stable-20250908-slim"
 ver="${debianVer}_baseimage"
-
-buildah bud --no-cache -f ./dockerfile-debian-stable-slim -t $URI/$AUUSER/$imgNAME:$ver 
+timeBuild=$(date +"%Y%m%d")
+buildah bud --no-cache -f ./dockerfile-debian-stable-slim -t $URI/$AUUSER/$imgNAME:$timeBuild_$ver 
 buildah tag $URI/$AUUSER/$imgNAME:$ver
 buildah push $URI/$AUUSER/$imgNAME:$ver
