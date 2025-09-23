fd018bc8d5623bee61b6df175a4d34bdafc4e4b2
彩霞姐姐 <support@e2bank.cn>
2025-09-23 14:16:55 +0000

    refactor(Dockerfile): 替换docker为buildah并优化镜像构建流程
    移除对debian基础镜像的重复构建和推送
    简化RUN指令中的软件包安装步骤
    启用SSH服务的默认启动命令

--- Dockerfile/build-base.sh
+++ Dockerfile/build-base.sh
@@ -2,13 +2,9 @@
 URI="docker.io"
 AUUSER="aspnmy"
 imgNAME="debian-ssh"
-imgNAMEBase="debian"
 s6OverlayVer="v3.2.1.0"
 debianVer="stable-20250908-slim"
 ver="s6_overlay_v${s6OverlayVer}_${debianVer}_baseimage"
 
-docker build --no-cache -f ./dockerfile-ssh-s6-overlay-baseimage \
-        -t $URI/$AUUSER/$imgNAME:$ver .
-docker tag $URI/$AUUSER/$imgNAME:$ver $URI/$AUUSER/$imgNAMEBase:$ver
-docker push $URI/$AUUSER/$imgNAME:$ver
-docker push $URI/$AUUSER/$imgNAMEBase:$ver
+buildah bud --no-cache -f  ./dockerfile-ssh-s6-overlay-baseimage -t $URI/$AUUSER/$imgNAME:$ver 
+buildah push $URI/$AUUSER/$imgNAME:$ver
