6d693a8ad523b61bf56995a25ac26b5bbdae93b7
彩霞姐姐 <support@e2bank.cn>
2025-09-23 14:37:05 +0000

    refactor(Dockerfile): 简化镜像版本标签命名
    移除版本标签中的冗余"baseimage"后缀，使标签更简洁

--- Dockerfile/build-base.sh
+++ Dockerfile/build-base.sh
@@ -5,7 +5,7 @@ imgNAME="debian-ssh"
 s6OverlayVer="3210"
 debianVer="stable-20250908-slim"
 timeBuild=$(date +"%Y%m%d%H%M%S")
-ver="${timeBuild}_s6_overlay_v${s6OverlayVer}_${debianVer}_baseimage"
+ver="${timeBuild}_s6_overlay_v${s6OverlayVer}_${debianVer}"
 
 buildah bud --no-cache -f  ./dockerfile-ssh-s6-overlay-baseimage -t $URI/$AUUSER/$imgNAME:$ver 
 buildah push $URI/$AUUSER/$imgNAME:$ver
