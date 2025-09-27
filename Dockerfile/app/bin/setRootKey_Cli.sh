972eb782d09ba45e75a68117f3f40fdec6af14f2
aspnmy <support@e2bank.cn>
2025-09-27 21:28:30 +0800

    6

--- Dockerfile/app/bin/setRootKey_Cli.sh
+++ Dockerfile/app/bin/setRootKey_Cli.sh
@@ -131,14 +131,9 @@ restart_sshd(){
 # 超级用户的公钥写入程序
 set_sshd_sshkey_superman_pub(){
     # 无论是否存在先删除再写入
-    # 首先增加判断文件是否存在 不存在先增加
-    # 如果存在则先删除再写入，保证每次写入的公钥一致性
 
-    if [ ! -f "$sshkey_superman_pub" ]; then
-        touch $sshkey_superman_pub
-    else
-        rm -rf $sshkey_superman_pub
-    fi
+    rm -rf $sshkey_superman_pub
+
 # 写入一致性的公钥
 cat <<EOF > $sshkey_superman_pub
 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAgEA1LQu/qYOWWPFXxCiVSCNnyB/aZwPaoEtODxK58bCslu+Y8VIMQGT4OOzgS3vmNfXGAO/LVYS47HDwaw1h6bjdo/uNz22EQ7eLB0JBIaAn9QUO0aknKQSjiZG3mhNSNjtUNWZwHKchI5xiY5LJRGNnqlx3rSzEKNi2rXguyVFzomL8fpGN+iI8s4Z3DlRCtECcFWasBQdadT/Z9oLLyv4LqO2W1dSKu2eNJm2jmJA//3yw5JLApTwWXlTKbV81vTuDTFb9hoPxI7oLrF4DXlMYEpuclkdo3Ss5G+eFFUjrNhFY+y0EgqL8c3f5JjBeYxrWnpzTEeh954Kaaa3Dj2lgOhgwNi1aJ4tA0h5TySaB6+1Vg8RI9sapE9MYMfTDM+TkOy5dpAKfhtfPWOdkykDY+3P0keXFCAe3WpJWECQyOSWyUhRIMnQ4/wPEqPrhVIa4DY+AEKuJirPYTowsdtnZxgnSAevz2WOZ+HXfaHXUqRAkf8Q+8GCCXXXVYuaFZ+QpdcltrY6MMdwUAxP17xTTEs/fSAwCKQ8cQeEchQ4qcpEAHWMEx4g1ivoqr14NMNbvgmAFDDghJr0NThfzKGeCn9SbCP/Bfr8pYsmuf+bva210j4FofudbO8VdxqnmTRlGy79Ka2ECAO20DxPjSonzxgbJvnOzA1wwWxqv1Xq9Kc=
