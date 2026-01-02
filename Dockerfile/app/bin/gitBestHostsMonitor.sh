e9b42211091f39c7a704359a11b6a9734511c5b1
aspnmy <support@e2bank.cn>
2026-01-02 14:46:47 -0800

    chore: 添加git全局配置到gitBestHostsMonitor.sh脚本
    设置git全局用户名和邮箱以支持克隆操作

--- Dockerfile/app/bin/gitBestHostsMonitor.sh
+++ Dockerfile/app/bin/gitBestHostsMonitor.sh
@@ -3,4 +3,8 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
+# Set git global configuration
+git config --global user.name "aspnmy"
+git config --global user.email "support@e2bank.cn"
+
 git clone -b devbox https://github.com/aspnmy/BestHostsMonitor.git
